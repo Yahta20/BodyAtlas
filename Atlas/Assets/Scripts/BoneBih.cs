4c6041f74b97c8459cf86ee146167e8ec8595969
Yahta20 <yahta2v0more@gmail.com>
2021-01-15 17:03:53 +0200

    uniRx+bonebehavior

---
+++ Atlas/Assets/Scripts/BoneBih.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+
+public class BoneBih : MonoBehaviour
+{
+    public Collider boneCollider;
+    public List<Transform> specPlases;
+    public bool chosen { get; private set; }
+    public Material currentMat { get; private set; }
+
+    void Awake() {
+        boneCollider = this.gameObject.GetComponent<Collider>();
+        //currentMat = 
+        foreach (Transform t in transform) {
+            specPlases.Add(t);
+            //print(t.name);
+        }
+        chosen = false;
+        currentMat = GetComponent<Renderer>().material;
+
+    }
+       // Start is called before the first frame update
+    void Start()
+    {
+        boneCollider.OnMouseDownAsObservable()
+            .Subscribe(s=> { 
+               //print("clic");
+               chosen = chosen == false ? true : false;
+            });
+    }
+
+       
+
+    void changeMaterial(Material mat)
+    {
+        currentMat = mat;
+    }
+
+}
